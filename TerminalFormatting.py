09cf8bff227a29d5e69c32232f5f259eba00fd1d
Vineel303 <vineel303@gmail.com>
2024-12-04 23:35:24 +0530

    0

---
+++ TerminalFormatting.py
@@ -0,0 +1,27 @@
+#variables
+#reset
+Reset = "\033[0m"
+
+#bold and dim
+Bold = "\033[1m"
+Dim = "\033[2m"
+
+#colours
+Red = "\033[31m"
+Green = "\033[32m"
+Blue = "\033[34m"
+Yellow = "\033[33m"
+Magenta = "\033[35m"
+Cyan = "\033[36m"
+
+#functions
+def specialPrint(String, Attribute): # to use multiple attributes at once, join them into a single string
+    global Reset
+    print(f"{Attribute}{String}{Reset}")
+
+def specialInput(String, AttributeForPrint, AttributeForInput): # to use multiple attributes at once, join them into a single string
+    global Reset
+    print(f"{AttributeForPrint}{String}{Reset}{AttributeForInput}", end="")
+    userInput = input()
+    print(Reset, end="")
+    return userInput
