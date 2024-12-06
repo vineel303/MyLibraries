5c6f9aece9e09196a709071bbd65984dd3d1cebf
Vineel303 <vineel303@gmail.com>
2024-12-06 23:14:27 +0530

    01388 U-Null

--- vinTextTools.py
+++ vinTextTools.py
@@ -1,4 +1,4 @@
 from deep_translator import GoogleTranslator
 
-def translateText(textToTranslate, inputLanguage="auto", outputLanguage="en"):
+def translateText(textToTranslate, inputLanguage="ja", outputLanguage="en"):
     return GoogleTranslator(source=inputLanguage, target=outputLanguage).translate(textToTranslate)
